5da01f197afad45b277c48519ea5665918029d5f
krueger-matt <krueger.matt@gmail.com>
2020-08-02 17:48:40 -0700

    Updated Get Score to send last 30 day results

--- get_score.py
+++ get_score.py
@@ -1,4 +1,5 @@
-# This checks if anyone has texted Get Score and responds with the current score from the score_keeper table
+# This checks if anyone has texted Get Score and responds with the current score as well as the last 30 day score 
+# from the score_keeper table 
 
 import sqlite3
 
@@ -10,21 +11,43 @@ def get_score(text):
 	if text.strip().lower() == "get score":
 
 		score_list = []
+		score_last_30_days = []
 
-		conn = sqlite3.connect(config.DB_NAME)
-		cursor = conn.execute("""SELECT e.name, count(sk.id) 
+		sql_score_list = """SELECT e.name, count(sk.id) 
 								 FROM score_keeper sk 
 								 JOIN emails e ON sk.email = e.email 
-								 GROUP BY 1 ORDER BY 2 DESC""")
+								 GROUP BY 1 ORDER BY 2 DESC"""
 
-		print ('Overall score:')
+		sql_last_30_list = """SELECT e.name, count(sk.id) 
+								 FROM score_keeper sk
+								 JOIN emails e ON sk.email = e.email
+								 WHERE timestamp >= date('now','-30 days')
+								 GROUP BY 1 ORDER BY 2 DESC"""
 
-		for row in cursor:
-			print (str(row[0]) + ": " + str(row[1]))
-			score_list.append(str(row[0]) + ": " + str(row[1]))
+		sqls = [sql_score_list, sql_last_30_list]
+		lists = [score_list, score_last_30_days]
 
-		email_body = str(score_list)
-		email_subject = "Overall score:"
+		conn = sqlite3.connect(config.DB_NAME)
+
+		x = 0
+
+		for sql in sqls:
+			cursor = conn.execute(sql)
+
+			if x == 0:
+				print ('Overall score:')
+			elif x ==1:
+				print ('Last 30 days:')
+
+			for row in cursor:
+				print (str(row[0]) + ": " + str(row[1]))
+				lists[x].append(str(row[0]) + ": " + str(row[1]))
+
+			x+=1
+
+		email_body = 'Overall Score:\n' + str(score_list) + '\n' + 'Last 30 Days:\n' + str(score_last_30_days)
+
+		email_subject = "Plant Bot Scores:"
 
 		plant_functions.send_email(email_subject,email_body)
 
